e77e38c2ade52752d8ee4450acae30c45623beb8
acheco <80710861+acheco@users.noreply.github.com>
2024-10-31 22:27:22 -0400

    implementing server action to insert invoices in the database

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,7 +8,6 @@ import {LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton} from "@/app
 
 export default async function Page() {
 
-
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
